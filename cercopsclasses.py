e1761aacbaa2bb32218d850cf7279b850559935f
Jacob Folsom <cercops@gmail.com>
2020-03-28 02:35:07 -0400

    fuck

--- cercopsclasses.py
+++ cercopsclasses.py
@@ -73,9 +73,56 @@ class intSet(object):
             if i in other.vals:
                 intersection.insert(i)
         return intersection
-  
+
     def __len__(self):
         return len(self.vals)
+
+class Container(object):
+    """ Holds hashable objects. Objects may occur 0 or more times """
+    def __init__(self):
+        """ Creates a new container with no objects in it. I.e., any object
+            occurs 0 times in self. """
+        self.vals = {}
+    def insert(self, e):
+        """ assumes e is hashable
+            Increases the number times e occurs in self by 1. """
+        try:
+            self.vals[e] += 1
+        except:
+            self.vals[e] = 1
+    def __str__(self):
+        s = ""
+        for i in sorted(self.vals.keys()):
+            if self.vals[i] != 0:
+                s += str(i)+":"+str(self.vals[i])+"\n"
+        return s
+
+class Bag(Container):
+    def remove(self, e):
+        """ assumes e is hashable
+            If e occurs in self, reduces the number of
+            times it occurs in self by 1. Otherwise does nothing. """
+        if e in self.vals:
+            self.vals[e] -= 1
+
+    def count(self, e):
+        """ assumes e is hashable
+            Returns the number of times e occurs in self. """
+        if e in self.vals:
+            return self.vals[e]
+        else:
+            return 0
+
+d1 = Bag()
+d1.insert(4)
+print(d1)
+d1.remove(4)
+print(d1)
+d1.remove(4)
+print(d1)
+d1.remove(4)
+print(d1)
+
 
 
 
